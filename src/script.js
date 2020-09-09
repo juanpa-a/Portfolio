1218138c3cbb026f6d23503aa9854da7504afcb0
Juan Pablo Ascencio <jpabloascencioglez@gmail.com>
2020-09-08 22:39:22 -0500

    Added current job to projects

--- src/script.js
+++ src/script.js
@@ -1,25 +1,3 @@
-// Horror movie way to animate
-
-// $("#about-button").on("click",function () {
-//     $('html, body').animate({
-//         scrollTop: ($('#about').offset().top)
-//     }, 1000);
-// });
-
-// $("#projects-button").on("click",function () {
-//     $('html, body').animate({
-//         scrollTop: ($('#projects').offset().top)
-//     }, 1000);
-// });
-
-// $("#contact-button").on("click",function () {
-//     $('html, body').animate({
-//         scrollTop: ($('#contact').offset().top)
-//     }, 1000);
-// });
-
-// nice way to do it :)
-
 $(document).on('click', '.nav-link', function() {
     let div = $(this).attr('id').slice(0, -7);
     $('html, body').animate({
@@ -27,9 +5,13 @@ $(document).on('click', '.nav-link', function() {
     }, 1500);
 })
 
-
-
 let projects = [
+    { 
+        name : 'Curatech',
+        screenshot : 'assets/img/curatech.png',
+        url : 'https://curatech.mx',
+        summary : "Telemedicine application, that helps doctor get in touch with their patients and monetize their time and knowledge.",
+    },
     { 
         name : 'Hangman',
         screenshot : 'assets/img/hangman.png',
@@ -47,7 +29,7 @@ let projects = [
         screenshot : 'assets/img/sortVis.png',
         url : 'https://juanpa-a.github.io/SortVis/',
         summary : "Visulizer to help you understand how sortin algorithms work.",
-    }
+    },
 ];
 
 for (p of projects) {
